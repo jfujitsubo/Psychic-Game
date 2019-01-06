0e1f423af2753a9fc5382000e8636a12090d05f6
Fooji <jfujitsubo@yahoo.com>
2019-01-05 19:40:42 -0800

    third commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
+//Can't figure out why the logic isnt working,
+//so didnt bother to add bootstrap or styles yet
+
+
 //Random Number
 var randomLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
